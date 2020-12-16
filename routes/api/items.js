ddb62a74fc1a92bcc8dff420a96a11678909a647
daniel simonson <danielsimonson@daniels-MacBook-Pro.local>
2020-12-17 02:51:25 +0800

    changed routes and default.json

--- routes/api/items.js
+++ routes/api/items.js
@@ -1,21 +1,20 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 //Item model
-const Item = require('../../models/Item')
+const Item = require("../../models/Item");
 
 // @route GET api/items
 // @desc Get all Items
 // @access Public
-router.get('/', (req, res) => {
-  Item.find()
-    .then(items => res.json(items))
-})
+router.get("/", (req, res) => {
+  Item.find().then((items) => res.json(items));
+});
 
 // @route POST api/items
 // @desc Create an item
 // @access Public
-router.post('/', (req, res) => {
+/*router.post('/', (req, res) => {
   const newItem = new Item({
     title: req.body.title,
     description: req.body.description,
@@ -23,14 +22,14 @@ router.post('/', (req, res) => {
     url: req.body.url
   })
   newItem.save().then(item => res.json(item))
-})
+})*/
 
 // @route delete api/items/:id
 // @desc delete a item
 // @access Public
-router.delete('/:id', (req, res) => {
+/*router.delete('/:id', (req, res) => {
   Item.findById(req.params.id)
     .then(item => item.remove().then(() => res.json({success: true})))
     .catch(err => res.status(404).json({success: true}))
-  })
-module.exports = router
+  })*/
+module.exports = router;
